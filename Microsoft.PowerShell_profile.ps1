86820830185b7ac6710c1d1d7f0afb34a679b267
Lam Tu <lam.tu@xero.com>
2021-05-26 14:04:58 +1000

    Add instructions on how to set up powershell

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,65 @@
+# Install starship if you want this
+# Invoke-Expression (&starship init powershell)
+
+# these functions relies on PSFzf: https://github.com/kelleyma49/PSFzf
+Import-Module PSFzf
+
+# Git aliases 
+function gs() {
+    git status
+}
+
+function gfr() {
+    git pull -r
+}
+
+function gca() {
+    if ([string]::IsNullOrWhiteSpace(${args})) {
+        Write-Host "Commit message is required" -Foreground Red
+        return
+    }
+
+    $coauthorsFilePath = "$HOME\.git-co-authors"
+    if (! (Test-Path $coauthorsFilePath)) {
+        Write-Host "File $coauthorsFilePath does not exist" -Foreground Red
+        return
+    }
+
+    $selected = Get-Content $coauthorsFilePath |
+            Invoke-FzF -Multi -Exit0 -Layout reverse |
+            ForEach-Object {
+                "co-authored-by: $_"
+            }
+
+    if ($selected.Length -eq 0) {
+        Write-Host "No coauthor selected" -Foreground Red
+        return
+    }
+
+    $coauthorsMessage = $selected -join "`n"
+    git commit -m "${args}`n`n${coauthorsMessage}"
+}
+
+function gco() {
+    git checkout $args
+}
+
+function g() {
+    git $args
+}
+
+# Kubect aliases
+function k() {
+    kubectl $args
+}
+
+function kl() {
+	$selected = kubectl get pods $args | Invoke-Fzf -Select1 -Exit0 -Layout reverse
+
+	if ([string]::IsNullOrWhitespace($selected)) {
+        Write-Host "No pods matches the provided pattern" -Foreground Yellow
+    } else {
+        kubectl logs -f $selected.split(" ")[0]
+    }	
+}
+
---
+++ README.md
@@ -0,0 +1,5 @@
+# Set up powershell
+
+1. Install https://github.com/kelleyma49/PSFzf
+2. Run `$profile` in ps1 to get your profile path
+3. Copy `Microsoft.PowerShell_profile.ps1` into your `$profile` path
